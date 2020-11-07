b86515c72ad619f3562d250d1ad6197e481a9914
otapi <otapi@protonmail.ch>
2020-11-07 16:23:48 +0100

    Install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+# Run this script on a fresh install of VM (Ubuntu) or run the commands manually
+# Disable auto-lock: https://techdirectarchive.com/2020/05/05/how-to-disable-automatic-screen-lock-in-ubuntu-linux/
+sudo dpkg --configure -a
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo apt-get install git
+
+# Generate the SSH key 
+ssh-keygen -t rsa -b 4096
+echo Copy the content of your public SSH key, it is the file id_rsa.pub by default
+cat id_rsa.pub
+echo Paste the content into your GitHub/BitBucket account on the SSH key section
+read -p "Press any key to resume ..."
+echo First clone
+git clone git@github.com:otapi/salescrawler.git
