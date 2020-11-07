e7b045615313f22979c4170ab6c2413abaf26a94
otapi <otapi@protonmail.ch>
2020-11-07 23:28:42 +0100

    install update

--- install_local.bat
+++ install_local.bat
@@ -1,3 +1,3 @@
-rem setup local Windows 10 machine for development
+rem setup local Windows 10 machine for development (but not run the code)
 rem #1 - install C++ build tools: https://visualstudio.microsoft.com/visual-cpp-build-tools/
 python -m pip install scrapy
