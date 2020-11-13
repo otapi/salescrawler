d1b6275bdb232c90ed63a5cc7148f54dea920cf5
otapi <otapi@protonmail.ch>
2020-11-13 23:42:28 +0100

    extraID

--- salesCrawlerScrapy/spiders/hardverapro.py
+++ salesCrawlerScrapy/spiders/hardverapro.py
@@ -11,7 +11,10 @@ class Hardverapro(scrapy.Spider):
             self.start_urls = [f'https://hardverapro.hu/aprok/keres.php?stext={searchterm}&county=&stcid=&settlement=&stmid=&minprice=&maxprice=&company=&cmpid=&user=&usrid=&selling=1&buying=1&stext_none=']
         if fullink:
             self.start_urls = [f'{fullink}']
-        self.spiderbotID = spiderbotID
+        if type(spiderbotID) == str:
+            self.spiderbotID = int(spiderbotID)
+        else: 
+            self.spiderbotID = spiderbotID
     
     def parse(self, response):
         for item in response.xpath("//li[@class='media']"):
@@ -23,7 +26,8 @@ class Hardverapro(scrapy.Spider):
                 price = Helpers.getNumber(item.xpath(".//div[@class='uad-info']/div[@class='uad-price']/text()").get()),
                 currency = 'HUF',
                 location = item.xpath(".//div[@class='uad-info']/div[@class='uad-light']/text()").get(),
-                spiderbotID = self.spiderbotID
+                spiderbotID = self.spiderbotID,
+                extraID = None
             )
 
         next_page = response.xpath("//li[@class='nav-arrow']/a[@rel='next']/@href").get()
