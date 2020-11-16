b5ffe4f8b2e96978f77ceaf44fd52767ed7de4ed
otapi <otapi@protonmail.ch>
2020-11-16 13:39:24 +0100

    xtw

--- main.py
+++ main.py
@@ -22,6 +22,73 @@ def index():
 
     return render_template('index.html', table=table)
 
+import wtforms
+
+from wtforms.widgets import HTMLString, html_params
+class XEditableWidget(object):
+    def __call__(self, field, **kwargs):
+        # get Field from FieldList and create x-editable link based on it 
+        subfield = field.pop_entry()
+        value = kwargs.pop("value", "")
+        
+        kwargs.setdefault('data-role', 'x-editable')
+        kwargs.setdefault('data-url', '/')
+        
+        kwargs.setdefault('id', field.matchid)
+        kwargs.setdefault('name', field.name)
+        kwargs.setdefault('href', '#')
+        
+        if not kwargs.get('pk'):
+            raise Exception('pk required')
+        kwargs['data-pk'] = kwargs.pop("pk")
+        
+        if isinstance(subfield, wtforms.StringField):
+            kwargs['data-type'] = 'text'
+        elif isinstance(subfield, wtforms.BooleanField):
+            kwargs['data-type'] = 'select'
+        elif isinstance(subfield, wtforms.RadioField):
+            kwargs['data-type'] = 'select'
+        elif isinstance(subfield, wtforms.SelectField):
+            kwargs['data-type'] = 'select'
+        elif isinstance(subfield, wtforms.DateField):
+            kwargs['data-type'] = 'date'
+        elif isinstance(subfield, wtforms.DateTimeField):
+            kwargs['data-type'] = 'datetime'
+        elif isinstance(subfield, wtforms.IntegerField):
+            kwargs['data-type'] = 'number'
+        elif isinstance(subfield, wtforms.TextAreaField):
+            kwargs['data-type'] = 'textarea'
+        else:
+            raise Exception('Unsupported field type: %s' % (type(subfield),))
+            
+        return HTMLString('<a %s>%s</a>' % (html_params(**kwargs), value))
+
+
+class XEditableForm(wtforms.Form):
+    # min_entries=1 is required, because XEditableWidget needs at least 1 entry
+    test1 = wtforms.FieldList(wtforms.StringField(), widget=XEditableWidget(), min_entries=1)
+    test2 = wtforms.FieldList(wtforms.StringField(), widget=XEditableWidget(), min_entries=1)
+
+
+@app.route('/matches2', methods=['POST', 'GET'])
+def index():
+    form = XEditableForm(request.form)
+    if (request.method == "POST") and form.validate():
+        for x in form:
+            # last_index will be set if a field is submitted
+            if getattr(x, 'last_index', None):
+                model = models.Match.query.get(x.last_index)
+                setattr(model, x.name, x.data.pop())
+                db.session.commit()
+    elif (request.method == "POST") and not form.validate():
+        print "Errors", form.errors
+        
+    for x in models.Match.query.all():
+        print x.test1, x.test2
+    return render_template('example.html', form=form)
+
+
+
 @app.route('/matches', methods=['GET', 'POST'])
 def matches():
   matches = db.get('matches')
