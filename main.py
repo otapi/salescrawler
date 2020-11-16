557a24ba9ad89e4661db340e7810d8edf39a60ed
otapi <otapi@protonmail.ch>
2020-11-16 21:48:40 +0100

    hide

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import flash, render_template, request, redirect
+from flask import flash, render_template, request, redirect, url_for
 from formencode import variabledecode
 from app import app
 from app import db
