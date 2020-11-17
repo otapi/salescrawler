d6e9ab78d28e5fa66b4961fe9c9ed1c5abd689de
otapi <otapi@protonmail.ch>
2020-11-17 15:51:06 +0100

    sd

--- forms.py
+++ forms.py
@@ -1,8 +1,10 @@
 from wtforms import Form, StringField, SelectField, DecimalField, BooleanField
 from salesCrawlerScrapy.settings import SPIDERS
 
-class SpidersForm(Form):
+class SpiderbotForm(Form):
     select = SelectField('Select spider:', choices=SPIDERS)
-    search = StringField('Search:')
+    search = StringField('Name:')
 
+class CrawlerForm(Form):
+    search = StringField('Name:')
 
