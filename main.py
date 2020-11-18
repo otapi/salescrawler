c63a5e3358601b17fb32ad6d6b2597b2b77905d9
otapi <otapi@protonmail.ch>
2020-11-18 14:05:29 +0100

    s

--- main.py
+++ main.py
@@ -99,8 +99,8 @@ def spiderbots(crawlerid):
     crawler = models.Crawler.query.filter_by(crawlerid=crawlerid).first()
     return render_template('spiderbots.html', spiderbots=spiderbots, crawler=crawler)
 
-@app.route('/new_crawler/<crawlerid>', methods=['GET', 'POST'])
-def new_crawler(crawlerid):
+@app.route('/new_spiderbots/<crawlerid>', methods=['GET', 'POST'])
+def new_spiderbots(crawlerid):
     form = forms.SpiderbotForm(request.form)
     if request.method == 'POST' and form.validate():
         if not ('from_spiderbots' in request.form):
