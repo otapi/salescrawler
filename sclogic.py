095c97e219858aaf1273543e23469e5261574e27
otapi <otapi@protonmail.ch>
2020-11-20 12:01:33 +0100

    as

--- sclogic.py
+++ sclogic.py
@@ -97,7 +97,6 @@ def update():
 
     click.echo('Get from github...')
     shutil.rmtree(os.path.join(Path.home(),'salescrawler'))
-    input("waiting...")
     os.chdir(Path.home())
     os.system('git clone git@github.com:otapi/salescrawler.git')
 
